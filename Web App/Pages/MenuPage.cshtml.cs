cc13306b5a94994066afe144411b27eca3d70c31
2214148 <2214148@CCR114MID24867>
2023-11-23 12:16:33 +0000

    Display Menu Items on the Menu Page

--- Web App/Pages/MenuPage.cshtml.cs
+++ Web App/Pages/MenuPage.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Web_App.Data;
+using Web_App.Models;
 
 namespace Web_App.Pages
 {
     public class MenuModel : PageModel
     {
+        private readonly Web_AppContext _context;
+        public IList<FoodItem> FoodItem { get; set; } = default!;
+
+
+        public MenuModel(Web_AppContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+            FoodItem = _context.FoodItems.FromSqlRaw("Select * FROM FoodItem").ToList();
         }
+
     }
 }
