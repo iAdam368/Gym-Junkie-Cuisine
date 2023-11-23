063ad4139a213b6d2cd5d2d12db7cf55cc19ca43
2214148 <2214148@CCR114MID24867>
2023-11-23 14:11:10 +0000

    Admin Page Added

---
+++ Web App/Pages/Admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_App.Pages.Menu
+{
+    public class AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Web App/To-Do-List.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Web_App
-{
-    public class To_Do_List
-    {
-        // Increase the size of food description in model and ERD
-        // Desktop item
-        //
-    }
-}
