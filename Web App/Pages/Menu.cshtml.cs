212b33d1f892a95d7d03c1d7a85cd6a2c63a9be1
2214148 <2214148@CCR114MID24867>
2023-11-28 17:39:38 +0000

    Added Search for Menu Page

--- Web App/Pages/Menu.cshtml.cs
+++ Web App/Pages/Menu.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Web_App.Data;
 using Web_App.Models;
@@ -11,16 +12,26 @@ namespace Web_App.Pages
         private readonly Web_AppContext _context;
         public IList<FoodItem> FoodItem { get; set; } = default!;
 
-
         public MenuModel(Web_AppContext context)
         {
             _context = context;
         }
+
+        [BindProperty]
+        public string Search { get; set; }
 
         public void OnGet()
         {
             FoodItem = _context.FoodItems.FromSqlRaw("Select * FROM FoodItem").ToList();
         }
 
+        public IActionResult OnPostSearch()
+        {
+            // Adjusted code to prevent SQL injection (previously an exception was thrown when entering ')
+            string queryString = "SELECT * FROM FoodItem WHERE FoodName LIKE @Search";
+            SqlParameter parameter = new SqlParameter("@Search", "%" + Search + "%");
+            FoodItem = _context.FoodItems.FromSqlRaw(queryString, parameter).ToList();
+            return Page();
+        }
     }
 }
