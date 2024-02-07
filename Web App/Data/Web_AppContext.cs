59835fb6fda1242264cd23994ab95ed2c936a930
Adam <Adam@DESKTOP-415LO5R>
2024-02-07 17:17:01 +0000

    Login System Update 1

--- Web App/Data/Web_AppContext.cs
+++ Web App/Data/Web_AppContext.cs
@@ -7,6 +7,8 @@ using Web_App.Models;
 
 namespace Web_App.Data
 {
+
+    // Add identity to the context 
     public class Web_AppContext : DbContext
     {
         public Web_AppContext (DbContextOptions<Web_AppContext> options) : base(options)
@@ -16,6 +18,7 @@ namespace Web_App.Data
         public DbSet<Web_App.Models.FoodItem> FoodItems { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // More code required here
             modelBuilder.Entity<FoodItem>().ToTable("FoodItem");
         }
     }
