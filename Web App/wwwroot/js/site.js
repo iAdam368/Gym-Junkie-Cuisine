d6b4152f1376852723d0cb126e7a7a125b529f3a
Adam <Adam@DESKTOP-415LO5R>
2024-03-18 20:56:51 +0000

    'Back to Top' Javascript button implemented

--- Web App/wwwroot/js/site.js
+++ Web App/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// JavaScript Code
 
-// Write your JavaScript code.
+// Code adapted from a YouTube video Developer Filip, 2021
+// Method to scroll to the top of the page
+// Implemented using a button that appears when scrolled down the page
+document.getElementById("to_top_button").style.display = 'none'
+window.onscroll = () => {
+    if (window.scrollY >= 400) {
+        document.getElementById("to_top_button").style.display = 'unset';
+    }
+
+    if (window.scrollY <= 200) {
+        document.getElementById("to_top_button").style.display = 'none';
+    }
+}
+const toTop = () => window.scrollTo({ top: 0, behavior: 'smooth' })
