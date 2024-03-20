c60e057b60636fbff195ea9a54c13ff88298720e
Adam <Adam@DESKTOP-415LO5R>
2024-03-20 01:29:33 +0000

    Creating the checkout page

---
+++ Web App/Pages/Checkout.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_App.Pages
+{
+    public class CheckoutModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+
+
+    }
+}
